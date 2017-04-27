36173977ed359e8ec832763df7c467d481c87b84
Valeri Vasquez <vnvasquez@berkeley.edu>
2017-04-27 11:26:14 -0700

    add consumption block parameters

--- jump-version.jl
+++ jump-version.jl
@@ -16,9 +16,22 @@ LZ = [20.,90.] # initial labor demand
 LSZ = sum(LZ) # labor endowment
 PKZ = 1. # initial capital price
 PLZ = 1. # initial labor price (wages)
-PDZ = [1.,1.] # inital commodity price
+PDZ = [1.,1.] # inital commodity price (price of domestically-produced commodities)
 IOZ = [5. 40. ; 15. 20.] # technical coefficients for intermediate inputs
 XDZ = (PKZ.*KZ)./PDZ + (PLZ.*LZ)./PDZ + (IOZ[1:2]*PDZ + IOZ[3:4]*PDZ)./PDZ # initial commodity production level
 sigmaF = [0.8, 1.2]# elasticity of substition between factors in production function
 gammaF = 1/(1+(1+((PLZ/PKZ)*((KZ./LZ)^(-1/sigmaF))))) # distribution parameter of capital
 aF = XDZ ./ (gammaF.*KZ.^((sigmaF-1)/sigmaF)+(1-gammaF).*LZ^((1-sigmaF)./sigmaF))^(sigmaF/(sigmaF-1)) # efficiency parameter in production function
+
+
+## Consumption block
+
+CZ = [55.,165.]    # household demand for commodities
+UZ			           # utility level of household
+YZ			           # household's total income
+frisch			       # Frisch parameter
+elasY = [0.9,1.1]  #income elasticities of demand for commodities
+
+io(sec, secc)	# Technical coefficients
+muH(sec)		# Subsistence level
+alphaHLES(sec)	# marginal budget shares of utility function
