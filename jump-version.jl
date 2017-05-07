b7bd26df6c2197be1cee96dac860460cfa89bef9
Valeri Vasquez <vnvasquez@berkeley.edu>
2017-05-07 16:09:44 -0700

    Establish indices to read in the data

--- jump-version.jl
+++ jump-version.jl
@@ -3,18 +3,19 @@ using JuMP
 # to do: add index; read in data
 
 M = model(IpoptSolver())
+sec = [:sec1, :sec2]
 
 ### PARAMETERS ###
 
 ## Production block
 
-KZ = [60.,50.]              # inital capital demand
+KZ[sec] = [60.,50.]              # inital capital demand
 KSZ = sum(KZ)               # capital endowment
-LZ = [20.,90.]              # initial labor demand
+LZ[sec] = [20.,90.]              # initial labor demand
 LSZ = sum(LZ)               # labor endowment
 PKZ = 1.                    # initial capital price
 PLZ = 1.                    # initial labor price (wages)
-PDZ = [1.,1.]               # inital commodity price (price of domestically-produced commodities)
+PDZ[sec] = [1.,1.]               # inital commodity price (price of domestically-produced commodities)
 IOZ = [5. 40. ; 15. 20.]    # intermediate inputs
 io = IOZ/XDZ               # Technical coefficients
 XDZ = (PKZ.*KZ)./PDZ + (PLZ.*LZ)./PDZ + (IOZ[1:2]*PDZ + IOZ[3:4]*PDZ)./PDZ
