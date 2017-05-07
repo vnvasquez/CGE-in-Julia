f070d98afd249b7ad399352fdbe76e4e224a1b59
Valeri Vasquez <vnvasquez@berkeley.edu>
2017-05-07 16:23:11 -0700

    Push interim changes to indices

--- jump-version.jl
+++ jump-version.jl
@@ -16,11 +16,11 @@ LSZ = sum(LZ)               # labor endowment
 PKZ = 1.                    # initial capital price
 PLZ = 1.                    # initial labor price (wages)
 PDZ[sec] = [1.,1.]               # inital commodity price (price of domestically-produced commodities)
-IOZ = [5. 40. ; 15. 20.]    # intermediate inputs
+IOZ[sec,com] = [5. 40. ; 15. 20.]    # intermediate inputs
 io = IOZ/XDZ               # Technical coefficients
 XDZ = (PKZ.*KZ)./PDZ + (PLZ.*LZ)./PDZ + (IOZ[1:2]*PDZ + IOZ[3:4]*PDZ)./PDZ
                             # initial commodity production level
-sigmaF = [0.8, 1.2]         # elasticity of substition between factors in production function
+sigmaF[sec] = [0.8, 1.2]         # elasticity of substition between factors in production function
 gammaF = 1/(1+(1+((PLZ/PKZ)*((KZ./LZ)^(-1/sigmaF)))))
                             # distribution parameter of capital
 aF = XDZ ./ (gammaF.*KZ.^((sigmaF-1)/sigmaF)+(1-gammaF).*LZ^((1-sigmaF)./sigmaF))^(sigmaF/(sigmaF-1))
@@ -28,10 +28,10 @@ aF = XDZ ./ (gammaF.*KZ.^((sigmaF-1)/sigmaF)+(1-gammaF).*LZ^((1-sigmaF)./sigmaF)
 
 
 ## Consumption block (household = consumer and provider of labor)
-CZ = [55.,165.]                          # household demand for commodities
+CZ[sec] = [55.,165.]                          # household demand for commodities
 YZ = PKZ*KSZ + PLZ*LSZ			             # household's total income
 frisch = -1.1			                       # Frisch parameter
-elasY = [0.9,1.1]                        # income elasticities of demand for commodities
+elasY[sec] = [0.9,1.1]                        # income elasticities of demand for commodities
 alphaHLES = (elasY.*(PDZ.*CZ.))/YZ	       # marginal budget shares of utility function
 muH = CZ + (alphaHLES.*YZ)/(PDZ.*frisch) 	            # Subsistence level
 UZ = prod(CZ. - muH.).^alphaHLES	       # utility level of household
@@ -64,12 +64,12 @@ end
 ## Consumption
 @constraints M begin
   # HOUSEHOLDS
-  EQC, C == muH + alphaHLES/(PD * (Y - sum(PD * muH))    	 #consumer consumption
+  EQC, C[sec] == muH[sec] + alphaHLES[sec]/(PD[sec] * (Y - sum(PD[sec] * muH[sec]))    	 #consumer consumption
   EQY, Y == PK*KS + PL*LS	                                 #income balance
-  EQU, U == prod((C - muH)^alphaHLES)	                     #household utility
+  EQU, U == prod((C[sec] - muH[sec])^alphaHLES[sec])	                     #household utility
 
   # MARKET CLEARING
-  EQXD, XD == sum(io * XD) + C	                           #market clearing consumption
+  EQXD, XD[sec] == sum(io[sec, com] * XD) + C	[sec]                           #market clearing consumption
 
 end
 
