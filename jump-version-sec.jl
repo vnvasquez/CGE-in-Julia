2792314e359594992dbc8c96745c601db3e0f593
Valeri Vasquez <vnvasquez@berkeley.edu>
2017-05-08 16:51:36 -0700

    Duplicate file to assist trouble shooting; remove SEC references in original

--- jump-version-sec.jl
+++ jump-version-sec.jl
@@ -9,18 +9,18 @@ sec = [:sec1, :sec2]
 
 ## Production block
 
-KZ = [60.,50.]              # inital capital demand
+KZ[sec] = [60.,50.]              # inital capital demand
 KSZ = sum(KZ)               # capital endowment
-LZ  = [20.,90.]              # initial labor demand
+LZ[sec] = [20.,90.]              # initial labor demand
 LSZ = sum(LZ)               # labor endowment
 PKZ = 1.                    # initial capital price
 PLZ = 1.                    # initial labor price (wages)
-PDZ  = [1.,1.]               # inital commodity price (price of domestically-produced commodities)
+PDZ[sec] = [1.,1.]               # inital commodity price (price of domestically-produced commodities)
 IOZ[sec,com] = [5. 40. ; 15. 20.]    # intermediate inputs
 io = IOZ/XDZ               # Technical coefficients
 XDZ = (PKZ.*KZ)./PDZ + (PLZ.*LZ)./PDZ + (IOZ[1:2]*PDZ + IOZ[3:4]*PDZ)./PDZ
                             # initial commodity production level
-sigmaF  = [0.8, 1.2]         # elasticity of substition between factors in production function
+sigmaF[sec] = [0.8, 1.2]         # elasticity of substition between factors in production function
 gammaF = 1/(1+(1+((PLZ/PKZ)*((KZ./LZ)^(-1/sigmaF)))))
                             # distribution parameter of capital
 aF = XDZ ./ (gammaF.*KZ.^((sigmaF-1)/sigmaF)+(1-gammaF).*LZ^((1-sigmaF)./sigmaF))^(sigmaF/(sigmaF-1))
@@ -28,10 +28,10 @@ aF = XDZ ./ (gammaF.*KZ.^((sigmaF-1)/sigmaF)+(1-gammaF).*LZ^((1-sigmaF)./sigmaF)
 
 
 ## Consumption block (household = consumer and provider of labor)
-CZ  = [55.,165.]                          # household demand for commodities
+CZ[sec] = [55.,165.]                          # household demand for commodities
 YZ = PKZ*KSZ + PLZ*LSZ			             # household's total income
 frisch = -1.1			                       # Frisch parameter
-elasY  = [0.9,1.1]                        # income elasticities of demand for commodities
+elasY[sec] = [0.9,1.1]                        # income elasticities of demand for commodities
 alphaHLES = (elasY.*(PDZ.*CZ.))/YZ	       # marginal budget shares of utility function
 muH = CZ + (alphaHLES.*YZ)/(PDZ.*frisch) 	            # Subsistence level
 UZ = prod(CZ. - muH.).^alphaHLES	       # utility level of household
@@ -64,29 +64,29 @@ end
 ## Consumption
 @constraints M begin
   # HOUSEHOLDS
-  EQC, C  == muH  + alphaHLES /(PD  * (Y - sum(PD  * muH ))    	 #consumer consumption
+  EQC, C[sec] == muH[sec] + alphaHLES[sec]/(PD[sec] * (Y - sum(PD[sec] * muH[sec]))    	 #consumer consumption
   EQY, Y == PK*KS + PL*LS	                                 #income balance
-  EQU, U == prod((C  - muH )^alphaHLES )	                     #household utility
+  EQU, U == prod((C[sec] - muH[sec])^alphaHLES[sec])	                     #household utility
 
   # MARKET CLEARING
-  EQXD, XD  == sum(io  * XD) + C	                            #market clearing consumption
+  EQXD, XD[sec] == sum(io[sec, com] * XD) + C	[sec]                           #market clearing consumption
 
 end
 
 ## Production
 @constraints M begin
   # FIRMS
-  EQK, K 	== gammaF ^sigmaF  * PK^(-sigmaF ) * (gammaF ^sigmaF  * PK^(1-sigmaF ) +
-        (1-gammaF )^sigmaF  * PL^(1-sigmaF ))^(sigmaF /(1-sigmaF )) * (XD /aF )                #firm demand for capital
+  EQK, K[sec]	== gammaF[sec]^sigmaF[sec] * PK^(-sigmaF[sec]) * (gammaF[sec]^sigmaF[sec] * PK^(1-sigmaF[sec]) +
+        (1-gammaF[sec])^sigmaF[sec] * PL^(1-sigmaF[sec]))^(sigmaF[sec]/(1-sigmaF[sec])) * (XD[sec]/aF[sec])                #firm demand for capital
 
-  EQL, L  == (1-gammaF )^sigmaF  * PL^(-sigmaF ) * (gammaF ^sigmaF  * PK^(1-sigmaF ) +
-        (1-gammaF )^sigmaF  * PL^(1-sigmaF )^(sigmaF /(1-sigmaF )) * (XD /aF )                 #firm demand for labor
+  EQL, L[sec] == (1-gammaF[sec])^sigmaF[sec] * PL^(-sigmaF[sec]) * (gammaF[sec]^sigmaF[sec] * PK^(1-sigmaF[sec]) +
+        (1-gammaF[sec])^sigmaF[sec] * PL^(1-sigmaF[sec])^(sigmaF[sec]/(1-sigmaF[sec])) * (XD[sec]/aF[sec])                 #firm demand for labor
 
-  EQZPC, PD  * XD  == PK*K  + PL*L  + sum(io *PD)*XD                                          #zero-profit condition
+  EQZPC, PD[sec] * XD[sec] == PK*K[sec] + PL*L[sec] + sum(io[sec, com]*PD)*XD[sec]                                         #zero-profit condition
 
   # MARKET CLEARING
-  EQKS, sum(K ) == KS           #capital market clearing  == sum(K)
-  EQLS, sum(L ) == LS           #labor market clearing == sum(L)
+  EQKS, sum(K[sec]) == KS           #capital market clearing  == sum(K)
+  EQLS, sum(L[sec]) == LS           #labor market clearing == sum(L)
 end
 
 ### SOLVER ###
