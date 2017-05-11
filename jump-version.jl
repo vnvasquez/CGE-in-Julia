7468683e4865686c651ef729b1cba9517a28f532
Cecilia Springer <ceciliahspringer@gmail.com>
2017-05-11 10:54:59 -0700

    Finish debugging

--- jump-version.jl
+++ jump-version.jl
@@ -16,9 +16,9 @@ PKZ = 1.                    # initial capital price
 PLZ = 1.                    # initial labor price (wages)
 PDZ  = data[:PDZ]           # inital commodity price (price of domestically-produced commodities)
 IOZ = [5. 40. ; 15. 20.]    # intermediate inputs
-XDZ = (PKZ.*KZ)./PDZ + (PLZ.*LZ)./PDZ + [sum(IOZ[1:2,]),sum(IOZ[3:4,])].*PDZ
+XDZ = (PKZ.*KZ)./PDZ + (PLZ.*LZ)./PDZ + [sum(IOZ[:,1]),sum(IOZ[:,2])].*PDZ
                             # initial commodity production level
-io = IOZ./XDZ               # Technical coefficients
+io = IOZ./XDZ'           # Technical coefficients
 sigmaF  = data[:sigmaF]     # elasticity of substition between factors in production function
 gammaF = 1./(1.+((PLZ/PKZ)*((KZ./LZ).^(-1./sigmaF))))
                             # distribution parameter of capital
@@ -31,8 +31,8 @@ CZ  = data[:CZ]                         # household demand for commodities
 YZ = PKZ*KSZ + PLZ*LSZ			            # household's total income
 frisch = -1.1			                      # Frisch parameter
 elasY  = data[:elasY]                   # income elasticities of demand for commodities
-alphaHLES = elasY.*((PDZ.*CZ)./YZ)
-alphaHLES = alphaHLES./sum(alphaHLES)   # marginal budget shares of utility function
+alphaHLES = (elasY.*((PDZ.*CZ)./YZ))./sum(elasY.*((PDZ.*CZ)./YZ))
+                                        # marginal budget shares of utility function
 muH = CZ + (alphaHLES.*YZ)./(PDZ.*frisch) 	            # Subsistence level
 UZ = prod((CZ - muH).^alphaHLES)	      # utility level of household
 
@@ -66,8 +66,6 @@ end
 
 ### EQUATIONS (constraints) ###
 
-## Production Block
-
 @NLconstraints M begin
 
   # FIRMS
@@ -75,7 +73,7 @@ end
         (1-gammaF[i])^sigmaF[i] * PL^(1-sigmaF[i]))^(sigmaF[i]/(1-sigmaF[i])) * (XD[i]/aF[i])                      # firm demand for capital
   EQL[i = sector], L[i] == (1-gammaF[i])^sigmaF[i] * PL^(-sigmaF[i]) * (gammaF[i]^sigmaF[i] * PK^(1-sigmaF[i]) + (1-gammaF[i])^sigmaF[i] * PL^(1-sigmaF[i]))^(sigmaF[i]/(1-sigmaF[i])) * (XD[i]/aF[i])
                                           # firm demand for labor
-  EQZPC[i = sector], PD[i] * XD[i] == PK*K[i] + PL*L[i] + sum(io[l,j] * PD[j] for l in sector, j in com)*XD[i]                              # zero-profit condition
+  EQZPC[i = sector], PD[i] * XD[i] == PK*K[i] + PL*L[i] + sum(io[l,i] * PD[l] for l in sector)*XD[i]                              # zero-profit condition
 
   # MARKET CLEARING
   EQKS, sum(K[j] for j in sector) == KS           # capital market clearing
@@ -88,13 +86,13 @@ end
 @NLconstraints M begin
 
   # HOUSEHOLDS
-  EQC[i = sector], C[i]  == muH[i]  + alphaHLES[i] /(PD[i]  * (Y - sum(PD[j]  * muH[j] for j in sector )))
+  EQC[i = sector], C[i]  == muH[i]  + (alphaHLES[i] /PD[i]) * (Y - sum(PD[j]  * muH[j] for j in sector ))
                                                                               # consumer consumption
   EQY, Y == PK*KS + PL*LS	                                                    # income balance
   EQU, U == prod((C[j]  - muH[j])^alphaHLES[j] for j in sector)	  # household utility
 
   # MARKET CLEARING
-  EQXD[i = sector], XD[i]  == sum(io[l,j]  * XD[j] for l in sector, j in com) + C[i]	                                                                      # market clearing consumption
+  EQXD[i = sector], XD[i]  == sum(io[i,j]  * XD[j] for j in sector) + C[i]	                                                                      # market clearing consumption
 
 end
 
